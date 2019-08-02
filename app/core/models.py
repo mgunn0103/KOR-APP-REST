edcba0f0aa9c5f049bb8e702d4368e6092fc5a0a
mgunn0103 <marcus.gunn@yahoo.com>
2019-08-01 23:05:46 -0400

    Added custom user model

---
+++ app/core/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        
+        """Creates and saves a new user"""
+
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        #specifying **extra_fields ensures that anything extra is captured here
+        # normalize_email is a method that comes with the base UserManager class
+        user = self.model(email=self.normalize_email(email), **extra_fields) 
+
+        # password is set like this so that it is not passed around in cleartext
+        user.set_password(password)
+
+        # this is required for supporting multiple databases
+        user.save(using=self.db) 
+
+        return user
+
+    def create_super_user(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
+        
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    """Custom user model that supports using email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # UserManager is assigned to the objects attribute of the model
+    objects = UserManager() 
+
+    # the USERNAME_FIELD must be a string (as described in the docs)
+    # We are pointing Django to the name of the field that it should use as an identifier
+    # The parent class is expecting a string
+    USERNAME_FIELD = 'email'
+
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@londonappdev.com'
+        password = 'Testpass123'
+
+        """ Calling the following get_user_model method actually references the settings file to see what 
+            has been set as the user model class (specified by the AUTH_USER_MODEL class). In our case, 
+            the user model class is set to the User class (core.User). In the User model, we set the "objects"
+            field/attribute to be the UserManager class. We then call the create_user method of that 
+            UserManager class to create new users.
+        """
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    # the first argument of every class method, including init is always a reference to the current instance of the class
+    def test_new_user_email_normalized(self):
+        """Test that the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_super_user(
+            'test@londonappdev.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
