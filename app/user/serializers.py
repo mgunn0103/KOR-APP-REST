c02f460e3d7d5e7817f29f4dae8fb37d0d75b359
mgunn0103 <marcus.gunn@yahoo.com>
2019-08-05 14:23:16 -0400

    added user updates and me endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -40,6 +40,19 @@ class UserSerializer(serializers.ModelSerializer):
         # this validated_data will be the JSON data from the POST
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        
+        return user
+
+
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
     email = serializers.CharField()
