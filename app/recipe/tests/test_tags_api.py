57d411e326383e2b746a6c750422fc2239550d02
mgunn0103 <marcus.gunn@yahoo.com>
2019-08-06 10:21:57 -0400

    Added create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -17,10 +17,10 @@ class PublicTagsApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        def test_login_required(self):
-            """Test that login is required for retrieving tags"""
-            response = self.client.get(TAGS_URL)
-            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED) 
+    def test_login_required(self):
+        """Test that login is required for retrieving tags"""
+        response = self.client.get(TAGS_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED) 
 
 class PrivateTagsApiTests(TestCase):
     """Test the authorized user tags API"""
@@ -61,3 +61,22 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name':''}
+        response = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
