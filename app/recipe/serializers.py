ee8a64db484b876efa131aabbdb4c75eb2ebbfc4
mgunn0103 <marcus.gunn@yahoo.com>
2019-08-07 16:35:54 -0400

    added custom recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,3 +36,10 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'price', 'link')
         # this just prevents the user from updating the primary key
         read_only_fields = ('id',)
+
+    
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    # lists the ingredients 
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,12 +5,24 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 # Format is <app>:<model>-<function>
 RECIPES_URL = reverse('recipe:recipe-list')
+
+def sample_tag(user, name='Main Course'):
+    """Create and return a sample tag"""
+    return Tag.objects.create(user=user, name=name)
+
+def sample_ingredient(user, name='Cinnamon'):
+    """Create and return a sample ingredient"""
+    return Ingredient.objects.create(user=user, name=name)
+
+def detail_url(recipe_id):
+    """Return recipe detial URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 # the double asterisks in the parameters mean that any additional values that are 
 # passed in will be available in a dictionary called params
@@ -84,5 +96,20 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        """Test viewing a recipe detail"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        response = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(response.data, serializer.data)
+
+        
+
 
 
